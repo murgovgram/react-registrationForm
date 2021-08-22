54080aa1d09aca25fb1324b51041903e4d14ccb1
murgovgram <murgovski@gmail.com>
2021-08-22 18:15:46 +0200

    update

--- src/components/Print.js
+++ src/components/Print.js
@@ -6,18 +6,18 @@ class Print extends Component {
         const { values: { username, firstName, lastName, date } } = this.props;
 
         return (
-            <div className="p-8 mt-20 border border-gray-200 rounded-lg shadow-2xl mx-96">
+            <div className="p-8 mt-20 text-left bg-white border border-gray-200 rounded-lg shadow-2xl mx-96">
                 <div className="my-6">
-                    <h1>Username: {username}</h1>
+                    <h1 className="font-bold">First Name:<span className="pl-8 italic">{firstName}</span></h1>
                 </div>
                 <div className="my-6">
-                    <h1>First Name: {firstName}</h1>
+                    <h1 className="font-bold">Last Name:<span className="pl-8 italic">{lastName}</span></h1>
                 </div>
                 <div className="my-6">
-                    <h1>Last Name: {lastName}</h1>
+                    <h1 className="font-bold">Username:<span className="pl-8 italic">{username}</span></h1>
                 </div>
                 <div className="my-6">
-                    <h1>Date of birth: {date}</h1>
+                    <h1 className="font-bold">Date of birth:<span className="pl-8 italic">{date}</span></h1>
                 </div>
             </div>
 
